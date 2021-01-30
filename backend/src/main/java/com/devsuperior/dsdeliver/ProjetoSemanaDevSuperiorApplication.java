3c646ef71903b68c3d8dcd52619a1d0f1cb9d23f
Walter Junior <wlt.junior92@gmail.com>
2021-01-30 19:51:14 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/ProjetoSemanaDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSemanaDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSemanaDevSuperiorApplication.class, args);
+	}
+
+}
