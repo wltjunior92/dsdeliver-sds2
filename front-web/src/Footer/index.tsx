ba03eb11e76e62fc7c405024f3f818a905980cac
Junior <wlt.junior92@gmail.com>
2021-02-15 16:51:22 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,23 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+export default function Footer() {
+  return (
+    <footer className="main-footer">
+      App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+      <div className="footer-icons">
+        <a href="https://youtube.com/c/DevSuperior" target="_new">
+          <YouTubeIcon />
+        </a>
+        <a href="https://linkedin.com/school/devsuperior" target="_new">
+          <LinkedinIcon />
+        </a>
+        <a href="https://instagram.com/devsuperior.ig/" target="_new">
+          <InstagramIcon />
+        </a>
+      </div>
+    </footer>
+  )
+}
